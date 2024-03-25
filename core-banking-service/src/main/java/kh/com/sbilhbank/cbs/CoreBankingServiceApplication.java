92966b0285c7e5ec59474d60775eb720d0463194
LONG Kongheng <kongheng.long@sbilhbank.com.kh>
2024-03-25 10:26:46 +0700

    feat: init core banking service

---
+++ core-banking-service/src/main/java/kh/com/sbilhbank/cbs/CoreBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package kh.com.sbilhbank.cbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBankingServiceApplication.class, args);
+	}
+
+}
